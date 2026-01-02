f0ba324b2952c28e1a957d38cbf07349a41fea13
cwissyboi <christopher.m.charlesworth@gmail.com>
2026-01-02 14:44:32 +0100

    Box plots per cluster

--- initial_pipeline/main.py
+++ initial_pipeline/main.py
@@ -50,10 +50,10 @@ def main():
             image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
             # Pipeline steps
-            yolo_boxes = yolo_inference(yolo, image_path, output_name = output_name,  output_to_file=True)
-            sam_masks = sam_inference(sam_predictor, yolo_boxes, image_path,  output_name = output_name, image_rgb = image_rgb, output_to_file=True)
-            soma_masks = get_gaussian_filter_soma_masks(yolo_boxes, image_path, image_rgb, output_name = output_name,  output_to_file= True)
-            skeletons = get_skeletons(image_rgb, image_path, sam_masks, soma_masks, output_to_file = True, output_name = output_name)
+            yolo_boxes = yolo_inference(yolo, image_path, output_name = output_name,  output_to_file=False)
+            sam_masks = sam_inference(sam_predictor, yolo_boxes, image_path,  output_name = output_name, image_rgb = image_rgb, output_to_file=False)
+            soma_masks = get_gaussian_filter_soma_masks(yolo_boxes, image_path, image_rgb, output_name = output_name,  output_to_file= False)
+            skeletons = get_skeletons(image_rgb, image_path, sam_masks, soma_masks, output_to_file = False, output_name = output_name)
             results_df = get_morphology_dataframe(sam_masks, skeletons, soma_masks, yolo_boxes)
 
             # results_df["image_path"] = image_path
