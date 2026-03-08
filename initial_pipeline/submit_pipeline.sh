3489010a8d114bd3e7b1bda46421474b41a4e024
cwissyboi <christopher.m.charlesworth@gmail.com>
2026-03-08 22:02:55 +0100

    wip

--- initial_pipeline/submit_pipeline.sh
+++ initial_pipeline/submit_pipeline.sh
@@ -9,7 +9,7 @@
 
 set -euo pipefail
 
-SCAN_DIR="../../ScanData/All_scans_tiled"
+SCAN_DIR="../../ScanData/10_patients_GM_tiles"
 SCAN_LIST="scan_list.txt"
 
 # Build the scan list from subdirectories of the scan data folder
