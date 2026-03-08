df36cd33affa16c6d7560dfddcf21a9700389f7f
cwissyboi <christopher.m.charlesworth@gmail.com>
2026-03-08 22:24:00 +0100

    ready for first 10 patients attempt

--- initial_pipeline/run_pipeline_array.sh
+++ initial_pipeline/run_pipeline_array.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=microglia_pipeline
 #SBATCH --partition=general
 #SBATCH --qos=short
-#SBATCH --time=0:10:00
+#SBATCH --time=2:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
